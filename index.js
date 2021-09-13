1d14d20bbd244b74afae7ee818474c5296ffb645
Thapelo Mmakola <mthapelothapza@gmail.com>
2021-09-13 14:51:44 +0200

    changes

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
-
+const exphbs = require('express-handlebars');
 const app = express();
 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+app.use(express.static('public'));
+
 app.get('/', function(req, res) {
-    res.send('Setting bill app is up');
+    res.render('index');
 })
 
 app.post('/settings', function (req, res) {
