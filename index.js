44827996b6d7d846091b14216b0b1cbd232f1398
Thapelo Mmakola <mthapelothapza@gmail.com>
2021-09-13 23:44:04 +0200

    changes made

--- index.js
+++ index.js
@@ -2,30 +2,56 @@ const express = require("express");
 const exphbs = require('express-handlebars');
 const app = express();
 
+const SettingsBill = require('./settings-bill')
+const bodyParser = require('body-parser');
+
+const settingsBill = SettingsBill();
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-    res.render('index');
-})
+    res.render('index', { 
+        settings: settingsBill.getSettings(),
+        totals: settingsBill.totals()
+    });
+    
+});
 
 app.post('/settings', function (req, res) {
+     console.log(req.body);
 
+    settingsBill.setSettings({
+        callCost: req.body.callCost,
+        smsCost: req.body.smsCost,
+        warningLevel: req.body.warningLevel,
+        criticalLevel: req.body.criticalLevel
+    })
+    res.redirect('/');
 });
 
 app.post('/action', function (req, res) {
-    
+    settingsBill.recordAction(req.body.actionType)
+    res.redirect('/');
 });
 
 app.get('/actions', function (req, res) {
+    res.render('actions', {actions: settingsBill.actions() 
     
+    });
 });
 
-app.get('/actions/:type', function (req, res) {
-    
+app.get('/actions/:actionType', function (req, res) {
+    const actionType = req.params.actionType;
+    res.render('actions', {actions: settingsBill.actionsFor(actionType) 
+    })
 })
 
 const PORT = process.env.PORT || 3014
